38a31ea12c69b122e69ceb56b2b12d16bf9a29b0
Henrique Sousa <rickftime@gmail.com>
2017-06-30 00:09:46 -0300

    Fix da busca de multiplos arquivos
        - Agora a busca é feita para cada BibEntry selecionada

--- src/main/java/org/jabref/gui/filelist/FinderAction.java
+++ src/main/java/org/jabref/gui/filelist/FinderAction.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 
 public class FinderAction extends AbstractWorker {
@@ -24,33 +25,35 @@ public class FinderAction extends AbstractWorker {
     @Override
     public void run() {
         panel = JabRefGUI.getMainFrame().getCurrentBasePanel();
-        BibEntry entry = panel.getSelectedEntries().get(0);
-        panel.output(Localization.lang("Searching for file in this computer") + '.');
-        Optional<String> o = entry.getTitle();
-        String s = o.get();
-        Path startingDir = Paths.get(System.getProperty("user.home"));
-        String pattern = s + ".{pdf,docx,doc,ppt,pptx,odt,epub,mobi}"; // "(?i)" + ".(docx|pdf)"
-        Finder finder = new Finder(pattern);
-        try {
-            Files.walkFileTree(startingDir, finder);
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        List<BibEntry> entradas = panel.getSelectedEntries();
+
+        for( BibEntry entry : entradas ) {
+            panel.output(Localization.lang("Searching for file in this computer") + '.');
+            Optional<String> o = entry.getTitle();
+            String s = o.get();
+            Path startingDir = Paths.get(System.getProperty("user.home"));
+            String pattern = s + ".{pdf,docx,doc,ppt,pptx,odt,epub,mobi}"; // "(?i)" + ".(docx|pdf)"
+            Finder finder = new Finder(pattern);
+            try {
+                Files.walkFileTree(startingDir, finder);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            if (!finder.getPaths().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "The search has found a result for " + s + " in " + finder.getFirstPath());
+                FileListEntry flEntry = new FileListEntry("", "");
+
+                flEntry.setLink(finder.getFirstPath());
+                FileListTableModel model = new FileListTableModel();
+                entry.getField(FieldName.FILE).ifPresent(model::setContent);
+                model.addEntry(model.getRowCount(), flEntry);
+                String newVal = model.getStringRepresentation();
+
+                entry.setField(FieldName.FILE, newVal);
+            } else {
+                JOptionPane.showMessageDialog(null, "We didn't find any results for \"" + s + "\"");
+            }
         }
-        if (!finder.getPaths().isEmpty()) {
-            JOptionPane.showMessageDialog(null, "The search has found a result for " + s + " in " + finder.getFirstPath());
-            FileListEntry flEntry = new FileListEntry("", "");
-
-            flEntry.setLink(finder.getFirstPath());
-            FileListTableModel model = new FileListTableModel();
-            entry.getField(FieldName.FILE).ifPresent(model::setContent);
-            model.addEntry(model.getRowCount(), flEntry);
-            String newVal = model.getStringRepresentation();
-
-            entry.setField(FieldName.FILE, newVal);
-        } else {
-            JOptionPane.showMessageDialog(null, "We didn't find any results for \"" + s + "\"");
-        }
-
     }
 }
 
